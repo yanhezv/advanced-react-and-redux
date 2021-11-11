589f56faf9cb4b90ef269f02e48e13edc302386a
Omar Villanueva Sotelo <omar@humantech.pe>
2021-11-10 21:18:30 -0500

    add redux configuration for comments data

--- testing/src/index.js
+++ testing/src/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducers from 'reducers';
 
 import App from 'components/App';
 import reportWebVitals from 'reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={createStore(reducers, {})}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
