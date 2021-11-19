2ef613cd4daff50eef17345a414c62219404e756
Omar Villanueva Sotelo <omar@humantech.pe>
2021-11-18 22:09:55 -0500

    fetch comments and added to the list

--- testing/src/actions/index.js
+++ testing/src/actions/index.js
@@ -1,8 +1,18 @@
-import { SAVE_COMMENT } from 'actions/types';
+import axios from 'axios';
+import { FETCH_COMMENTS, SAVE_COMMENT } from 'actions/types';
 
 export function saveComment(comment) {
   return {
     type: SAVE_COMMENT,
     payload: comment,
+  };
+}
+
+export function fetchComments() {
+  const response = axios.get('https://jsonplaceholder.typicode.com/comments');
+
+  return {
+    type: FETCH_COMMENTS,
+    payload: response,
   };
 }
