0f569b8d9536f0fb90f0de2e380579d21fbcc757
Omar Villanueva Sotelo <omar@humantech.pe>
2021-11-19 00:11:04 -0500

    add header and sign in and sign out action

--- testing/src/components/App.js
+++ testing/src/components/App.js
@@ -1,15 +1,59 @@
-import React from "react";
-import CommentBox from "components/CommentBox";
-import CommentList from "components/CommentList";
-import { Route, Routes } from "react-router-dom";
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Link, Route, Routes } from 'react-router-dom';
 
-export default () => {
-  return (
-    <div>
-      <Routes>
-        <Route path="/post" element={<CommentBox />}/>
-        <Route path="/" element={<CommentList />}/>
-      </Routes>
-    </div>
-  );
+import CommentBox from 'components/CommentBox';
+import CommentList from 'components/CommentList';
+import * as actions from 'actions'
+
+class App extends Component {
+  renderButton() {
+    if (this.props.auth) {
+      return (
+        <button onClick={() => this.props.changeAuth(false)}>
+          Sign Out
+        </button>
+      );
+    } else {
+      return (
+        <button onClick={() => this.props.changeAuth(true)}>
+          Sign In
+        </button>
+      );
+    }
+  }
+
+  renderHeader() {
+    return (
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/post">Post a comment</Link>
+        </li>
+        <li>
+          {this.renderButton()}
+        </li>
+      </ul>
+    );
+  }
+
+  render() {
+    return (
+      <div>
+        {this.renderHeader()}
+        <Routes>
+          <Route path="/post" element={<CommentBox />} />
+          <Route path="/" element={<CommentList />} />
+        </Routes>
+      </div>
+    );
+  }
 };
+
+function mapStateToProps(state) {
+  return { auth: state.auth };
+}
+
+export default connect(mapStateToProps, actions)(App);
